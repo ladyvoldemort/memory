94c7752f8d39119661053c6bedc622b9210df5a0
Brittany V <brittbyu@gmail.com>
2018-07-24 15:58:54 -0600

    initial commit for submitting

---
+++ app.js
@@ -0,0 +1,24 @@
+$(document).ready( function() {
+
+  //create a grid of 12 "cards"-- Is there a grid array?
+
+  //hide the face side of the cards until clicked-- then show face
+
+  //show 12 copies of the back of the card from one image link-- What kind of function is this?
+
+  //flip one card function
+
+  //flip a second card function
+
+  //if ______ == ________ they stay flipped up
+
+  //if ______ != ________ flip back face down after milisecond
+
+  //create counter
+
+  //create function for 'new game' button that shuffles cards and resets the game
+  
+
+  
+
+})
